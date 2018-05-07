f209dadfec105d9a04f35c29b107375814b0af1c
Uglemat <uglemat@gmail.com>
2018-05-07 17:56:20 +0200

    Trimming tetromino stuff

--- tetrominoes.py
+++ tetrominoes.py
@@ -4,69 +4,48 @@ from collections import namedtuple
 X, O = 'X', None
 Tetromino = namedtuple("Tetrimino", "name color shape")
 
-T_long = Tetromino(name="long",
-                   color="blue",
-                   shape=((O,O,O,O),
-                          (X,X,X,X),
-                          (O,O,O,O),
-                          (O,O,O,O)))
-
-T_square = Tetromino(name="square",
-                     color="yellow",
-                     shape=((X,X),
-                            (X,X)))
-
-T_hat = Tetromino(name="hat",
-                  color="pink",
-                  shape=((O,X,O),
-                         (X,X,X),
-                         (O,O,O)))
-
-T_right_snake = Tetromino(name="right_snake",
-                          color="green",
-                          shape=((O,X,X),
-                                 (X,X,O),
-                                 (O,O,O)))
-
-T_left_snake = Tetromino(name="left_snake",
-                         color="red",
-                         shape=((X,X,O),
-                                (O,X,X),
-                                (O,O,O)))
-
-T_left_gun = Tetromino(name="left_gun",
-                       color="cyan",
-                       shape=((X,O,O),
-                              (X,X,X),
-                              (O,O,O)))
-
-T_right_gun = Tetromino(name="right_gun",
-                        color="orange",
-                        shape=((O,O,X),
-                               (X,X,X),
-                               (O,O,O)))
-
-list_of_tetrominoes = [T_long, T_square, T_hat,
-                       T_right_snake, T_left_snake, 
-                       T_left_gun, T_right_gun]
+list_of_tetrominoes = [
+    Tetromino(name="long",
+              color="blue",
+              shape=((O,O,O,O),
+                     (X,X,X,X),
+                     (O,O,O,O),
+                     (O,O,O,O))),
+    Tetromino(name="square",
+              color="yellow",
+              shape=((X,X),
+                     (X,X))),
+    Tetromino(name="hat",
+              color="pink",
+              shape=((O,X,O),
+                     (X,X,X),
+                     (O,O,O))),
+    Tetromino(name="right_snake",
+              color="green",
+              shape=((O,X,X),
+                     (X,X,O),
+                     (O,O,O))),
+    Tetromino(name="left_snake",
+              color="red",
+              shape=((X,X,O),
+                     (O,X,X),
+                     (O,O,O))),
+    Tetromino(name="left_gun",
+              color="cyan",
+              shape=((X,O,O),
+                     (X,X,X),
+                     (O,O,O))),
+    Tetromino(name="right_gun",
+              color="orange",
+              shape=((O,O,X),
+                     (X,X,X),
+                     (O,O,O)))
+    ]
 
 def rotate(shape, times=1):
     """ Rotate a shape to the right """
-    if times == 0:
-        return shape
+    return shape if times == 0 else rotate(tuple(zip(*shape[::-1])), times-1)
 
-    rotated = [[] for _ in range(len(shape))]
-
-    # Rotate one time to the right
-    for line in shape:
-        for index, atom in enumerate(line):
-            rotated[index].insert(0, atom)
-
-    return tuple(map(tuple, rotated)) if times <= 1 else rotate(rotated, times-1)
-
-def rotate_left(shape, times=1):
-    """ Rotate a shape to the left """
-    return rotate(shape, 3) if times <= 1 else rotate_left(rotate(shape, 3), times-1)
 
 def shape_str(shape):
     """ Return a string of a shape in human readable form """
