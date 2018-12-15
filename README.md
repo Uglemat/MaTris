54cc717b4c41981c8ae571657db2986118b3a578
Uglemat <uglemat@gmail.com>
2018-12-15 16:29:04 +0100

    Fixed tests and better README

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ MaTris
 
 A clone of Tetris made using Pygame. Licensed under the GNU GPLv3. Works with both Python 2 and 3.
 
-Run `python matris.py` to start the game.
+Run `python matris.py` (or `python3 matris.py`) to start the game.
+
+Requirements
+============
+
+The game requires [pygame](https://www.pygame.org). On Ubuntu it can be installed with these commands: `sudo apt install python-pip && sudo pip install pygame` (for Python 2) `sudo apt install python3-pip && sudo pip3 install pygame` (for Python 3).
 
 Demo
 ====
--- tetrominoes.py
+++ tetrominoes.py
@@ -2,45 +2,39 @@ from __future__ import print_function
 from collections import namedtuple
 
 X, O = 'X', None
-Tetromino = namedtuple("Tetrimino", "name color shape")
+Tetromino = namedtuple("Tetrimino", "color shape")
 
-list_of_tetrominoes = [
-    Tetromino(name="long",
-              color="blue",
-              shape=((O,O,O,O),
-                     (X,X,X,X),
-                     (O,O,O,O),
-                     (O,O,O,O))),
-    Tetromino(name="square",
-              color="yellow",
-              shape=((X,X),
-                     (X,X))),
-    Tetromino(name="hat",
-              color="pink",
-              shape=((O,X,O),
-                     (X,X,X),
-                     (O,O,O))),
-    Tetromino(name="right_snake",
-              color="green",
-              shape=((O,X,X),
-                     (X,X,O),
-                     (O,O,O))),
-    Tetromino(name="left_snake",
-              color="red",
-              shape=((X,X,O),
-                     (O,X,X),
-                     (O,O,O))),
-    Tetromino(name="left_gun",
-              color="cyan",
-              shape=((X,O,O),
-                     (X,X,X),
-                     (O,O,O))),
-    Tetromino(name="right_gun",
-              color="orange",
-              shape=((O,O,X),
-                     (X,X,X),
-                     (O,O,O)))
-    ]
+tetrominoes = {
+    "long": Tetromino(color="blue",
+                      shape=((O,O,O,O),
+                             (X,X,X,X),
+                             (O,O,O,O),
+                             (O,O,O,O))),
+    "square": Tetromino(color="yellow",
+                        shape=((X,X),
+                               (X,X))),
+    "hat": Tetromino(color="pink",
+                     shape=((O,X,O),
+                            (X,X,X),
+                            (O,O,O))),
+    "right_snake": Tetromino(color="green",
+                             shape=((O,X,X),
+                                    (X,X,O),
+                                    (O,O,O))),
+    "left_snake": Tetromino(color="red",
+                            shape=((X,X,O),
+                                   (O,X,X),
+                                   (O,O,O))),
+    "left_gun": Tetromino(color="cyan",
+                          shape=((X,O,O),
+                                 (X,X,X),
+                                 (O,O,O))),
+    "right_gun": Tetromino(color="orange",
+                           shape=((O,O,X),
+                                  (X,X,X),
+                                  (O,O,O)))
+}
+list_of_tetrominoes = list(tetrominoes.values())
 
 def rotate(shape, times=1):
     """ Rotate a shape to the right """
@@ -66,19 +60,19 @@ def test():
     map(shape,     tetromino_shapes)
     map(shape_str, tetromino_shapes)
 
-    assert shape_str(T_left_snake.shape) == "XXO\nOXX\nOOO"
+    assert shape_str(tetrominoes["left_snake"].shape) == "XXO\nOXX\nOOO"
 
-    assert rotate(T_square.shape) == T_square.shape
+    assert rotate(tetrominoes["square"].shape) == tetrominoes["square"].shape
 
-    assert rotate(T_right_snake.shape, 4) == T_right_snake.shape
+    assert rotate(tetrominoes["right_snake"].shape, 4) == tetrominoes["right_snake"].shape
 
-    assert rotate(T_hat.shape)    == ((O,X,O),
-                                      (O,X,X),
-                                      (O,X,O))
+    assert rotate(tetrominoes["hat"].shape)    == ((O,X,O),
+                                                   (O,X,X),
+                                                   (O,X,O))
 
-    assert rotate(T_hat.shape, 2) == ((O,O,O),
-                                      (X,X,X),
-                                      (O,X,O))
+    assert rotate(tetrominoes["hat"].shape, 2) == ((O,O,O),
+                                                   (X,X,X),
+                                                   (O,X,O))
     print("All tests passed in {}, things seems to be working alright".format(__file__))
 
 if __name__ == '__main__':
